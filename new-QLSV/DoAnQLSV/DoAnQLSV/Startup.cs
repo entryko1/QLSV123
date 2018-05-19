42ea3493eeb828aa99b208e1d76176763fab630c
Hieu Phan <bioshock.hieu@gmail.com>
2018-05-19 17:42:08 +0700

    Lấy cái này rồi ráp cái của m vào
    OK

---
+++ new-QLSV/DoAnQLSV/DoAnQLSV/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoAnQLSV.Startup))]
+namespace DoAnQLSV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
